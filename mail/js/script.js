14850832de7f8a4ff3248cf967dcaff9ac4a6364
expldec <explosive.deconstruction@gmail.com>
2022-04-12 13:17:54 +0200

    added comments to  mail exercise

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,14 +1,19 @@
-const userEmail = prompt('inserisci la tua mail');
+//dichiaro gli indirizzi email validi
 const validEmails = ['bill@microsoft.com','silvio@forzaitalia.it','joe@joebiden.com'];
-
+// chiedo all'utente il suo indirizzo email
+const userEmail = prompt('inserisci la tua mail');
+//dichiaro una variabile per determinare se l'utente "può accedere". Di default è false.
 userIsAllowed = false;
 
+//facciamo partire un loop che girerà tante volte quante sono le email nell'array delle email valide
 for (let i = 0; i < validEmails.length; i++) {
+    //se l'email dell'utente è tra quelle valide, allora può accedere.
     if (userEmail === validEmails[i]) {
         userIsAllowed = true;
     }
 }
 
+//con le informazioni ottenute nel loop, posso decidere se far entrare l'utente oppure dirgli di andare via.
 if (userIsAllowed) {
     alert('Benvenuto a bordo.');
 }
